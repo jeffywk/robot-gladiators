a4effdbe9d22ff30762cdf5d7bf9c2cae295803d
Apo11o <jeffknoespel@gmail.com>
2021-06-20 20:54:43 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,12 +19,13 @@ var fight = function(enemyName) {
             var confirmSkip = window.confirm("Are you sure you'd like to quit?");
             if (confirmSkip) {
                 window.alert(playerName + " has decided to skip this fight. Goodbye!");
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log("playerMoney", playerMoney);
                 break;
             }
         }
-            enemyHealth = enemyHealth - playerAttack;
+            var damage = randomNumber(playerAttack - 3, playerAttack);
+            enemyHealth = Math.max(0, enemyHealth - damage);
             console.log(
                 playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
             );
@@ -36,7 +37,8 @@ var fight = function(enemyName) {
                 window.alert(enemyName + " still has " + enemyHealth + " left.");
             }
     
-            playerHealth = playerHealth - enemyAttack;
+            var damage = randomNumber(enemyAttack - 3, enemyAttack);
+            playerHealth = Math.max(0, playerHealth - damage);
             console.log(
                 enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
             );
@@ -56,7 +58,7 @@ var startGame = function() {
     if (playerHealth > 0) {
         window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ));
         var pickedEnemyName = enemyNames[i];
-        enemyHealth = 50;
+        enemyHealth = randomNumber(40, 60);
      //   debugger;
         fight(pickedEnemyName);
         if (playerHealth > 0 && i < enemyNames.length - 1) {
@@ -118,4 +120,9 @@ var shop = function() {
             break;
     }
 };
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+    
+    return value;
+}
 startGame();
